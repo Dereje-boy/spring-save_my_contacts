8a4e76f04c8a7b870aa22a703ca34914888e6e33
Dereje-boy <Derejeg35@gmail.com>
2023-12-22 14:54:03 +0300

    now working on connecting sqlite db file from absolute path

--- src/main/java/com/dit/save_my_contacts/dao/ConnectionDB.java
+++ src/main/java/com/dit/save_my_contacts/dao/ConnectionDB.java
@@ -1,8 +1,11 @@
 package com.dit.save_my_contacts.dao;
 
+import com.dit.save_my_contacts.SaveMyContactsApplication;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Objects;
 
 public class ConnectionDB {
     public static final String DATABASE_URL = "jdbc:sqlite:database_name.db";
@@ -10,6 +13,14 @@ public class ConnectionDB {
     public static final String DB_PASSWORD = "";
 
     public Connection getConnection(){
+        try {
+            String dbPath = Objects.requireNonNull(ConnectionDB.class.getResource("")).toExternalForm().concat("database_name.db");
+
+            System.out.println(dbPath);
+        }catch (NullPointerException exception){
+            exception.printStackTrace();
+        }
+
         try{
             return DriverManager.getConnection(DATABASE_URL);
         }catch (SQLException sqlException){
